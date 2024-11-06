bc07e908350792863336216fa8021a586e33bdae
Yaseminnurlu <yaseminn@bu.edu>
2024-11-06 16:05:57 +0000

    saving progress

--- src/app.tsx
+++ src/app.tsx
@@ -3,18 +3,40 @@ import { Routes, Route, BrowserRouter } from 'react-router-dom';
 import HomePage from './components/Home'; // Import Home Component
 import Login from './components/Login'; // Import Login Component
 import Register from './components/Register'; // Import Register Component
+import List from './components/List'; // Import List Component
+import { useLocation } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
+function MainContent() {
+    const location = useLocation();
+    const showButtons = location.pathname === '/home' || location.pathname === '/List';
+
+    return (
+        <div>
+            {/* Conditionally render buttons based on the route */}
+            {showButtons && (
+                <div className="button-container">
+                    <Link to="/home"><button>Home</button></Link>
+                    <Link to="/List"><button>Saved pin</button></Link>
+    </div>
+)}
+
+            <Routes>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="*" element={<Register />} />
+                <Route path="/list" element={<List />} />
+            </Routes>
+        </div>
+    );
+}
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="*" element={<Register />} />
-          </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <MainContent />
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/List.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const List: React.FC<{}> = () => {
+    return (
+        <>
+        <h2 id="listTitle">Time and Date</h2>
+
+        </>
+    )
+};
+
+export default List
