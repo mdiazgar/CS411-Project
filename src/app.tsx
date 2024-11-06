afd43a7b258f5ceb5330b49660bee27708c7ab88
Yaseminnurlu <yaseminn@bu.edu>
2024-11-06 16:21:14 +0000

    saving progress

--- src/app.tsx
+++ src/app.tsx
@@ -1,20 +1,48 @@
 import React from 'react';
-import { Routes, Route, BrowserRouter } from 'react-router-dom';
+import { Routes, Route, BrowserRouter, useLocation } from 'react-router-dom';
 import HomePage from './components/Home'; // Import Home Component
 import Login from './components/Login'; // Import Login Component
 import Register from './components/Register'; // Import Register Component
+import { Link } from 'react-router-dom';
+import List from "./components/List";
 
+
+function MainContent() {
+    const location = useLocation();
+    const showButtons = location.pathname === '/home' || location.pathname === '/list';
+
+    return (
+        <div>
+            {/* Conditionally render buttons based on the route */}
+
+            {showButtons && (
+                <div className="button-container">
+                    <Link to="/home">
+                        <button>Maps</button>
+                    </Link>
+                    <Link to="/list">
+                        <button>Saved Pins</button>
+                    </Link>
+                </div>
+            )}
+
+            <Routes>
+                <Route path="/home" element={<HomePage />} />
+                <Route path="/login" element={<Login />} />
+                <Route path="/register" element={<Register />} />
+                <Route path="/list" element={<List />} />
+                <Route path="*" element={<Login />} />
+            </Routes>
+        </div>
+    );
+}
 
 function App() {
-  return (
-      <BrowserRouter>
-          <Routes>
-              <Route path="/home" element={<HomePage />} />
-              <Route path="/login" element={<Login />} />
-              <Route path="*" element={<Register />} />
-          </Routes>
-      </BrowserRouter>
-  );
+    return (
+        <BrowserRouter>
+            <MainContent />
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ src/components/list.tsx
@@ -0,0 +1,17 @@
+import React from "react"
+
+const List = () => {
+
+  
+    return (
+        <div>
+            <>
+            <h3>Saved pins</h3>
+            </>
+
+        </div>
+    );
+};
+  
+  export default List;
+  
